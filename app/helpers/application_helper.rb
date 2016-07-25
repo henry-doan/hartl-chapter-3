f129c719b95b55293bd178f0b896ab55f029db28
Henry Doan <henrydoan96@gmail.com>
2016-07-25 15:18:09 -0600

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	# return a title on a per page basis
+	def full_title(page_title)
+		base_title = "Hartl Chapter 3 sample app"
+		if page_title.empty?
+			base_title
+		else
+			"#{base_title} | #{page_title}"
+		end
+	end
 end
