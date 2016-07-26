5ade7ce31caefdc2fcebdeac7ca5c525da29c91d
Henry Doan <henrydoan96@gmail.com>
2016-07-26 17:09:09 -0600

    dry routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
 
-  get 'static_pages/help'
+  get 'static_pages/help', as: 'help'
 
-  get 'static_pages/about'
+  get 'static_pages/about', as: 'about'
 
-  get 'static_pages/contact'
+  get 'static_pages/contact', as: 'contact'
 
   get '/signup', to: 'users#new', as: 'signup'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
