5997d5964777116e377316b1b4c9a3c3ce5173a0
Henry Doan <henrydoan96@gmail.com>
2016-08-01 10:33:04 -0600

    fixed controller and routes for followers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,7 +55,7 @@ class UsersController < ApplicationController
     render 'show_follow'
   end
 
-  def following
+  def followers
     @title = "Followers"
     @user = User.find(params[:id])
     @users = @user.followers.paginate(page: params[:page])
